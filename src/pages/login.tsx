6a7eaf6b3510fd1fa41d29c3b60d0541a1f376e0
chaNcharge <chancharge@gmail.com>
2023-11-07 17:00:12 -0800

    Save login info username to localStorage

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -3,6 +3,7 @@ import Form from "@/components/Form";
 import { sendQuery } from "@/lib/dbclient";
 import { useRouter } from "next/router";
 import { useState } from "react";
+import { saveToLocalStorage } from "@/lib/LocalStorage";
 
 
 export default function Login() { 
@@ -12,15 +13,18 @@ export default function Login() {
     async function handleLogin(username: string, password: string) {
         if (username.length > 0 && password.length > 0) {
             // Check if user already exists
-            const existingUserCheckContent = "SELECT * FROM `cmd_degree_guide`.`Users` WHERE `username` = ?;"
-            const existingUserCheckResponseObject = await sendQuery(existingUserCheckContent, username);
+            const existingUserCheckQuery = "SELECT * FROM `cmd_degree_guide`.`Users` WHERE `username` = ?;"
+            const existingUserResponseObject = await sendQuery(existingUserCheckQuery, username);
 
-            if (existingUserCheckResponseObject.response.length > 0
-                && existingUserCheckResponseObject.response[0].password === password) {
+            if (existingUserResponseObject.response.length > 0
+                && existingUserResponseObject.response[0].password === password) {
                 // User exists, login
-                // TODO: Save username to localStorage or cookie
+                saveToLocalStorage("loggedInUser", existingUserResponseObject[0].username);
                 setLoginMessage("Log in success! Redirecting in 3 seconds...");
-                // TODO: Set redirect
+                // Wait 3 seconds, then redirect
+                setTimeout(() => {
+                    router.push("/courseselection");
+                }, 3000)
             } else {
                 // Username and password are incorrect, stop
                 setLoginMessage("Incorrect username and/or password.")
