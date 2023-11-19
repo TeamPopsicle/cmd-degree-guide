92e93f5e8fb672122457742c0dba7fc30d3e1e19
Yaya Yao <yayayao2003@gmail.com>
2023-11-19 11:55:26 -0800

    input page modified a little

--- src/pages/input.tsx
+++ src/pages/input.tsx
@@ -7,6 +7,7 @@ export default function UserInput() {
     const [termsLeft, setTermsLeft] = useState(0);
     const [coursesTaken, setCoursesTaken] = useState("");
     const router = useRouter();
+    const [major, setMajor] = useState("");
 
     // TODO: Check to make sure user is logged in, redirect back to login if not on open.
     // Proposal: Use useEffect()
@@ -33,6 +34,9 @@ export default function UserInput() {
             height: "100vh",
             backgroundColor: "#fbf4cf", // Set the background color for the entire webpage
             padding: "20px", // Add some padding to the form
+            position: "relative", // Position relative for absolute positioning of the image
+            borderRadius: "15px",
+            boxShadow: "0 0 10px rgba(0, 0, 0, 0.1)",
         }}
         >
             <label style={{ marginBottom: "10px", color: "darkgreen", textAlign: "center", fontSize: "26px" }}>
@@ -50,12 +54,30 @@ export default function UserInput() {
                         borderRadius: "10px", // Add some border radius for a rounded look
                         padding: "8px", // Add padding for better visual appeal
                         marginTop: "5px", // Add margin to separate from the label
-                        
+                        fontSize: "20px",
                     }}
                 />
             </label>
 
-            <label style={{ marginBottom: "10px", color: "darkgreen", textAlign: "center", fontSize: "26px" }}>
+            <label style={{color: "darkgreen", textAlign: "center", fontSize: "26px" }}>
+                What is your major?
+                <br />
+                <input
+                    type="text"
+                    value={major}
+                    onChange={e => setMajor(e.target.value)}
+                     style={{
+                        border: "2px solid darkgreen", // Set the same color as the text
+                        borderRadius: "10px",
+                        padding: "8px",
+                        marginTop: "5px", // Add margin to separate from the label
+                        fontSize: "20px",
+                    }}
+                />
+                <br />
+            </label>
+
+            <label style={{color: "darkgreen", textAlign: "center", fontSize: "26px" }}>
                 Enter courses taken, each separated by space:
                 <br />
                 <input
@@ -67,14 +89,19 @@ export default function UserInput() {
                         borderRadius: "10px",
                         padding: "8px",
                         marginTop: "5px", // Add margin to separate from the label
+                        fontSize: "20px",
                     }}
                 />
+                <br />
             </label>
+            <label style= {{marginBottom: "60px", color: "darkgreen", textAlign: "center", fontSize: "20px" }}>
+                    (e.g: 210 211 212)
+                </label>
             <button type="submit"
              style={{
                 backgroundColor: "darkgreen",
                 color: "#fbf4cf",
-                padding: "10px 20px",
+                padding: "12px 24px",
                 fontSize: "20px",
                 border: "none",
                 borderRadius: "20px",
