64f664b924336d833c6579e3160a008607161b9f
chaNcharge <chancharge@gmail.com>
2023-11-22 17:27:27 -0800

    Change major selection to dropdown, add major to genalg

--- src/pages/input.tsx
+++ src/pages/input.tsx
@@ -8,54 +8,59 @@ export default function UserInput() {
     const [coursesTaken, setCoursesTaken] = useState("");
     const router = useRouter();
     const [major, setMajor] = useState("");
+    const [warningMessage, setWarningMessage] = useState("");
 
     // TODO: Check to make sure user is logged in, redirect back to login if not on open.
     // Proposal: Use useEffect()
-    // FIX ME: Need a different way to delimit the courses input by the user. Commas instead? 
+    // FIXME: Need a different way to delimit the courses input by the user. Commas instead? 
 
     function handleSubmit() {
         // Calculate algorithm here, then save the result of the algorithm to localStorage
         // TODO: Also save to User Database
         // TODO: Add error checking, e.g don't redirect and show error message if runGenAlg fails
-        saveToLocalStorage("schedule", runGenAlg(termsLeft, coursesTaken));
-                // Check the major and redirect accordingly
-        switch (major.toLowerCase()) {
-            case "computer science":
-                router.push("/compSciChecklist");
-                break;
-            case "data science":
-                router.push("/dataSciChecklist");
-                break;
-            case "math":
-                router.push("/mathChecklist");
-                break;
-            default:
-                // Redirect to a default page or show an error message
-                router.push("/defaultPage");
-                break;
-    }
+        if (major !== "") {
+            saveToLocalStorage("schedule", runGenAlg(termsLeft, coursesTaken, major));
+            // Check the major and redirect accordingly
+            switch (major.toLowerCase()) {
+                case "computer science":
+                    router.push("/compSciChecklist");
+                    break;
+                case "data science":
+                    router.push("/dataSciChecklist");
+                    break;
+                case "math":
+                    router.push("/mathChecklist");
+                    break;
+                default:
+                    // Redirect to a default page or show an error message
+                    router.push("/defaultPage");
+                    break;
+            }
+        } else {
+            setWarningMessage("Please choose a major before submitting.");
+        }
     }
     return (
         <form
-        onSubmit={e => {
-            e.preventDefault();
-            handleSubmit();
-        }}
-        style={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            justifyContent: "center",
-            height: "100vh",
-            backgroundColor: "#fbf4cf", // Set the background color for the entire webpage
-            padding: "20px", // Add some padding to the form
-            position: "relative", // Position relative for absolute positioning of the image
-            borderRadius: "15px",
-            boxShadow: "0 0 10px rgba(0, 0, 0, 0.1)",
-        }}
+            onSubmit={e => {
+                e.preventDefault();
+                handleSubmit();
+            }}
+            style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center",
+                height: "100vh",
+                backgroundColor: "#fbf4cf", // Set the background color for the entire webpage
+                padding: "20px", // Add some padding to the form
+                position: "relative", // Position relative for absolute positioning of the image
+                borderRadius: "15px",
+                boxShadow: "0 0 10px rgba(0, 0, 0, 0.1)",
+            }}
         >
             <label style={{ marginBottom: "10px", color: "darkgreen", textAlign: "center", fontSize: "26px" }}>
-                How many terms do you have until expected graduation? (max 12) 
+                How many terms do you have until expected graduation? (max 12)
                 <br />
                 <input
                     type="number"
@@ -74,46 +79,51 @@ export default function UserInput() {
                 />
             </label>
 
-            <label style={{color: "darkgreen", textAlign: "center", fontSize: "26px" }}>
+            <label style={{ color: "darkgreen", textAlign: "center", fontSize: "26px" }}>
                 What is your major?
                 <br />
-                <input
-                    type="text"
+                <select
                     value={major}
                     onChange={e => setMajor(e.target.value)}
-                     style={{
+                    style={{
                         border: "2px solid darkgreen", // Set the same color as the text
                         borderRadius: "10px",
                         padding: "8px",
                         marginTop: "5px", // Add margin to separate from the label
                         fontSize: "20px",
                     }}
-                />
+                >
+                    <option value="" disabled>--Choose a Major--</option>
+                    <option value={"CS"}>Computer Science</option>
+                    <option value={"DS"}>Data Science</option>
+                    <option value={"MA"}>Math</option>
+                </select>
                 <br />
             </label>
 
-            <label style= {{marginBottom: "60px", color: "darkgreen", textAlign: "center", fontSize: "20px" }}>
-                    (choose from: computer science, data science, math)
-                </label>
+            <label style={{ marginBottom: "60px", color: "darkgreen", textAlign: "center", fontSize: "20px" }}>
+                (choose from: computer science, data science, math)
+            </label>
             <button type="submit"
-             style={{
-                backgroundColor: "darkgreen",
-                color: "#fbf4cf",
-                padding: "12px 24px",
-                fontSize: "20px",
-                border: "none",
-                borderRadius: "20px",
-                cursor: "pointer",
-                marginTop: "15px",
-                transition: "background-color 0.3s", // Add transition effect on hover
-            }}
-            onMouseOver={(e) => {
-                (e.target as HTMLElement).style.backgroundColor = "green"; // Change background color on hover
-            }}
-            onMouseOut={(e) => {
-                (e.target as HTMLElement).style.backgroundColor = "darkgreen"; // Revert back to the original color on mouse out
-            }}
-        >Submit</button>
+                style={{
+                    backgroundColor: "darkgreen",
+                    color: "#fbf4cf",
+                    padding: "12px 24px",
+                    fontSize: "20px",
+                    border: "none",
+                    borderRadius: "20px",
+                    cursor: "pointer",
+                    marginTop: "15px",
+                    transition: "background-color 0.3s", // Add transition effect on hover
+                }}
+                onMouseOver={(e) => {
+                    (e.target as HTMLElement).style.backgroundColor = "green"; // Change background color on hover
+                }}
+                onMouseOut={(e) => {
+                    (e.target as HTMLElement).style.backgroundColor = "darkgreen"; // Revert back to the original color on mouse out
+                }}
+            >Submit</button>
+            {warningMessage && <p className="text-red-500">{warningMessage}</p>}
         </form>
     );
 }
